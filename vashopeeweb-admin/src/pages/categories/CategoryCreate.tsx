25361030e363d0e29e037f5029c3ce4ec262c9f9
hieudd <duchieu195@gmail.com>
2026-05-16 02:24:04 +0700

    fix: resolve antd v6 FormInstance type incompatibility in CategoryCreate

--- vashopeeweb-admin/src/pages/categories/CategoryCreate.tsx
+++ vashopeeweb-admin/src/pages/categories/CategoryCreate.tsx
@@ -8,13 +8,15 @@ export default function CategoryCreate() {
     redirect: 'list',
   });
 
+  const { form: _form, ...restFormProps } = formProps;
+
   const handleFinish = (values: Record<string, unknown>) => {
     return formProps.onFinish?.({ ...values, id: values.slug });
   };
 
   return (
     <Create saveButtonProps={saveButtonProps}>
-      <Form {...formProps} onFinish={handleFinish} layout="vertical">
+      <Form {...restFormProps} onFinish={handleFinish} layout="vertical">
         <Form.Item label="Tên danh mục" name="name" rules={[{ required: true }]}><Input /></Form.Item>
         <Form.Item label="Icon (emoji)" name="icon" rules={[{ required: true }]}><Input placeholder="💄" /></Form.Item>
         <Form.Item label="Slug" name="slug" rules={[{ required: true }]}><Input placeholder="son-moi" /></Form.Item>
